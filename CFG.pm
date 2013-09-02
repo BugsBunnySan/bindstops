e13c0dd1ad1a301aa905182696f6d7930524b4e3
Sebastian Haas <bugs.bunny.san@googlemail.com>
2013-09-02 14:23:36 +0200

    added copyright notices

--- CFG.pm
+++ CFG.pm
@@ -1,3 +1,13 @@
+# Copyright (c) 2013 Sebastian Haas
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY.
+
+# See the LICENSE file for more details.
+
 package CFG;
 use strict; 
 
--- bindstops.pl
+++ bindstops.pl
@@ -1,6 +1,20 @@
 #!/usr/bin/perl -w
 use strict;
 
+# BindStops
+# stops bind attacks using firewall rules
+#
+# Copyright (c) 2013 Sebastian Haas
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY.
+
+# See the LICENSE file for more details.
+
+
 ## standrd perl modules
 use NDBM_File;
 use Data::Dumper;
